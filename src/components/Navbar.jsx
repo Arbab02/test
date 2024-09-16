c85e6eed9e74a7df88a5e44e751487d69504e1eb
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-09-15 21:27:29 -0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,70 @@
 'use client'
 
-import React from 'react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import Link from 'next/link';
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
 const Navbar = () => {
+  // State to manage the navbar's visibility
+  const [nav, setNav] = useState(false);
+
+  // Toggle function to handle the navbar's display
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  // Array containing navigation items
+  const navItems = [
+    { id: 1, text: 'Home' },
+    { id: 2, text: 'Company' },
+    { id: 3, text: 'Resources' },
+    { id: 4, text: 'About' },
+    { id: 5, text: 'Contact' },
+  ];
+
   return (
-    <motion.header 
-      className="text-white bg-[#000319]  "
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.5 }}
-    >
-      <div className="lg:text-2xl  container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-        <motion.div 
-          className="flex-shrink-0 pt-1"
-          initial={{ x: -100 }}
-          animate={{ x: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <Image
-            src="/images/logo.png"
-            width={90}
-            height={90}
-            alt="Logo"
-          />
-        </motion.div>
+    <div className='bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+      {/* Logo */}
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
 
-        <motion.nav 
-          className="md:ml-auto md:mr-auto flex flex-wrap items-center text-lg justify-center"
-          initial={{ y: -50 }}
-          animate={{ y: 0 }}
-          transition={{ duration: 0.5, delay: 0.2 }}
-        >
-          <Link className="lg:text-xl mr-5 hover:text-purple-400 transition duration-300" href="/">Home</Link>
-          <Link className="lg:text-xl mr-5 hover:text-purple-400 transition duration-300" href="/About">About</Link>
-          <Link className="lg:text-xl mr-5 hover:text-purple-400 transition duration-300" href="/Portfolio">Portfolio</Link>
-          <Link className="lg:text-xl mr-5 hover:text-purple-400 transition duration-300" href="/Services">Services</Link>
-          <Link className="lg:text-xl mr-[-2rem] hover:text-purple-400 transition duration-300" href="/Contact">Contact Us</Link>
-        </motion.nav>
+      {/* Desktop Navigation */}
+      <ul className='hidden md:flex'>
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 hover:bg-[#00df9a] rounded-xl m-2 cursor-pointer duration-300 hover:text-black'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
 
-        <motion.button 
-          className="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0 transition duration-300"
-          initial={{ scale: 0.9 }}
-          animate={{ scale: 1 }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ duration: 0.3 }}
-        >
-        abc@gmail.com
-          
-        </motion.button>
+      {/* Mobile Navigation Icon */}
+      <div onClick={handleNav} className='block md:hidden'>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
-    </motion.header>
+
+      {/* Mobile Navigation Menu */}
+      <ul
+        className={
+          nav
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
+        }
+      >
+        {/* Mobile Logo */}
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+
+        {/* Mobile Navigation Items */}
+        {navItems.map(item => (
+          <li
+            key={item.id}
+            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
+          >
+            {item.text}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
