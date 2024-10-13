15b45251f4838520108f1a677ddff5915b104dc4
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-10-13 12:13:56 -0700

    Update Mission.jsx

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Mission = () => {
   return (
     <section className="text-gray-400 bg-[#000319] body-font">
-      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+      <div className="container mx-auto flex px-5 py-16 items-center justify-center flex-col">
         <img
           className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
